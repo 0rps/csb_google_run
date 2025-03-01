bc029ce42d0f3fef37cf96da0623e70f8a85dc99
Aleksey Mogilnikov <a.mogilnikov@criteo.com>
2025-03-02 00:41:57 +0530

    Fixes

--- 01/init.sh
+++ 01/init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-GCP_PROJECT_ID=qwiklabs-gcp-04-b44c8db33efd
-GCP_PROJECT_NUMBER="1096674639877"
-GCP_REGION=us-central1
+GCP_PROJECT_ID=-
+GCP_PROJECT_NUMBER="-"
+GCP_REGION=us-
 
 gcloud projects list
 gcloud config set project "$GCP_PROJECT_ID"
 
+gcloud services enable storage.googleapis.com
 
-sed -i "s|`<gcp_project>`|project-asdf-123|g" manifest.tf
-sed -i "s|\`<gcp_project_id>\`|$GCP_PROJECT_ID|g" manifest.tf
-sed -i "s|\`<gcp_project_number>\`|$GCP_PROJECT_NUMBER|g" manifest.tf
 
+sed -i "s|<gcp_project_id>|$GCP_PROJECT_ID|g" manifest.tf
+sed -i "s|<gcp_project_number>|$GCP_PROJECT_NUMBER|g" manifest.tf
+sed -i "s|<gcp_region>|$GCP_REGION|g" manifest.tf
--- 01/push_image.sh
+++ 01/push_image.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd ./lab03
 
-gcloud services enable storage.googleapis.com
+npm install express
+npm install body-parser
+npm install child_process
+npm install @google-cloud/storage
 
+PROJECT_ID=`gcloud config get project 2>/dev/null`
+
+gcloud builds submit --tag gcr.io/$PROJECT_ID/pdf-converter
